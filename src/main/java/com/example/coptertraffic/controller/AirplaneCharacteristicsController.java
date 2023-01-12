ce1396dad98d4a751445a737b763d7d9fc36f5e8
Serhii Buria <sergeiburya@gmail.com>
2023-01-12 11:23:00 +0200

    updade AirplaneCharacteristicsController

--- src/main/java/com/example/coptertraffic/controller/AirplaneCharacteristicsController.java
+++ src/main/java/com/example/coptertraffic/controller/AirplaneCharacteristicsController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/airplane-characteristic")
+@RequestMapping("/airplane-characteristics")
 public class AirplaneCharacteristicsController {
     private final AirplaneCharacteristicsService characteristicsService;
     private final AirplaneCharacteristicsMapper characteristicsMapper;
